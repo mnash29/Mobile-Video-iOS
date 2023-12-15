3735c48fdeef95e5457717241f4d3c0a91b445cf
mnash29 <mtntrades10@gmail.com>
2023-12-15 15:06:49 -0800

    Implemented signout capability

--- TikTok/Controllers/Other/SettingsViewController.swift
+++ TikTok/Controllers/Other/SettingsViewController.swift
@@ -9,10 +9,106 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    private let tableView: UITableView = {
+        let view = UITableView(frame: .zero, style: .grouped)
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        return view
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Settings"
         view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        createFooter()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+
+    func createFooter() {
+        let footer = UIView(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: view.width,
+            height: 100
+        ))
+        
+        let button = UIButton(frame: CGRect(
+            x: (view.width - 200) / 2,
+            y: 25,
+            width: 200,
+            height: 50
+        ))
+        
+        button.setTitle("Sign Out", for: .normal)
+        button.setTitleColor(.systemRed, for: .normal)
+        button.addTarget(self, action: #selector(didTapSignOut), for: .touchUpInside)
+        footer.addSubview(button)
+
+        tableView.tableFooterView = footer
+    }
+
+    @objc func didTapSignOut() {
+        let actionSheet = UIAlertController(
+            title: "Sign Out",
+            message: "Would you like to sign out?",
+            preferredStyle: .actionSheet
+        )
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        actionSheet.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { _ in
+            AuthManager.shared.signOut { [weak self] success in
+                DispatchQueue.main.async {
+                    if success {
+                        UserDefaults.standard.setValue(nil, forKey: "username")
+                        UserDefaults.standard.setValue(nil, forKey: "profile_image_url")
+
+                        let navVC = UINavigationController(rootViewController: SignInViewController())
+                        navVC.modalPresentationStyle = .fullScreen
+
+                        self?.present(navVC, animated: true)
+                        self?.navigationController?.popToRootViewController(animated: true)
+                        self?.tabBarController?.selectedIndex = 0
+                    }
+                    else {
+                        let alert = UIAlertController(title: "Woops", message: "Something went wrong while signing out. Please try again.", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+                        self?.present(alert, animated: true)
+                    }
+                }
+            }
+        }))
+
+        present(actionSheet, animated: true)
+    }
+
+}
+
+// MARK: - UITableViewDelegate methods
+
+extension SettingsViewController: UITableViewDelegate {
+
+}
+
+// MARK: - UITableViewDataSource methods
+
+extension SettingsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
     }
 
 }
